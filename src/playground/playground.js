e984ad0e58afc4640153c0e56d4d7eeb7cdc98e2
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-02-24 20:09:11 -0300

    Agregadas funciones de fecha en español

--- src/playground/playground.js
+++ src/playground/playground.js
@@ -8,14 +8,18 @@ console.log(date.es()); // 31/12/2021
 console.log(date.en()); // 12/31/2021
 console.log(date.isoShortEs()); // 31-12-2021
 console.log(date.isoShortEn()); // 2021-12-31
+console.log(date.isoShortPt()); // 2021-12-31
 console.log(date.getDay()); // 31
 console.log(date.getMonth()); // 12
 console.log(date.getYear()); // 2021
 console.log(date.getWeekDay()); // 5
 console.log(date.getWeekDayNameEs()); // Viernes
 console.log(date.getWeekDayNameEn()); // Friday
+console.log(date.getWeekDayNamePt()); // Sexta-feira
 console.log(date.getMonthNameEs()); // Diciembre
 console.log(date.getMonthNameEn()); // December
+console.log(date.getMonthNamePt()); // Dezembro
 console.log(date.getLongDateEs()); // 31 de Diciembre de 2021
 console.log(date.getLongDateEn()); // December 31, 2021
+console.log(date.getLongDatePt()); // 31 de Dezembro de 2021
 console.log(date.getDate()) // 2024-12-01T03:00:00.000Z (Date object)
--- src/playground/playground.ts
+++ src/playground/playground.ts
@@ -4,18 +4,22 @@ var date = new ChavetasoftDates('2024-12-1'); // you can pass a string as YYYY-M
 date = new ChavetasoftDates('2024/12/1'); // you can pass a string as YYYY/MM/DD
 date = new ChavetasoftDates(new Date('2024-12-1')); // you can pass a Date object
 
-console.log(date.es()); // 31/12/2021
-console.log(date.en()); // 12/31/2021
-console.log(date.isoShortEs()); // 31-12-2021
-console.log(date.isoShortEn()); // 2021-12-31
-console.log(date.getDay()); // 31
+console.log(date.es()); // 1/12/2024
+console.log(date.en()); // 12/1/2024
+console.log(date.isoShortEs()); // 1-12-2024
+console.log(date.isoShortEn()); // 2024-12-1
+console.log(date.isoShortPt()); // 2024-12-1
+console.log(date.getDay()); // 1
 console.log(date.getMonth()); // 12
-console.log(date.getYear()); // 2021
-console.log(date.getWeekDay()); // 5
-console.log(date.getWeekDayNameEs()); // Viernes
-console.log(date.getWeekDayNameEn()); // Friday
+console.log(date.getYear()); // 2024
+console.log(date.getWeekDay()); // 0
+console.log(date.getWeekDayNameEs()); // Domingo
+console.log(date.getWeekDayNameEn()); // Sunday
+console.log(date.getWeekDayNamePt()); // Domingo
 console.log(date.getMonthNameEs()); // Diciembre
 console.log(date.getMonthNameEn()); // December
-console.log(date.getLongDateEs()); // 31 de Diciembre de 2021
-console.log(date.getLongDateEn()); // December 31, 2021
+console.log(date.getMonthNamePt()); // Dezembro
+console.log(date.getLongDateEs()); // 1 de Diciembre de 2024
+console.log(date.getLongDateEn()); // December 1, 2024
+console.log(date.getLongDatePt()); // 1 de Dezembro de 2024
 console.log(date.getDate()); // 2024-12-01T03:00:00.000Z (Date object)
